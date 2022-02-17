e0b5e211f841a87c4a1b0857caec8a497a90a6fe
RickFreitas <henrique.manoel@hotmail.com.br>
2022-02-17 09:29:12 -0300

    count list length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,7 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  def call(list), do: count_list(list, 0)
+
+  def count_list([], count), do: count
+
+  def count_list([_head | tail], count), do: count_list(tail, count + 1)
 end
